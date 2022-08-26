80c7ccab3da7c839ac2bdc605c637cf48d444b01
sour <dahiya.sourabh444@hmail.com>
2022-08-26 22:13:22 +0530

    node.ejs sign in and sign up master branch

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const router=express.Router();
+
+const homeController=require('../controllers/home_controller');
+console.log('router loaded');
+router.get('/',homeController.home);
+//fir any fi=urther routed .access from here
+//router.use('/routerName',require('./routerfile'))'
+router.use('/users',require('./users'));
+module.exports=router;
